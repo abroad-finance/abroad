45affa7db993d53892cdaa18964d97f0ed2635e4
Hollwann <hollwann@gmail.com>
2025-08-14 11:32:55 +0000

    Comment out BinanceListener registration and startup in index.ts

--- src/listeners/index.ts
+++ src/listeners/index.ts
@@ -1,7 +1,7 @@
 // src/listeners/index.ts
 
 import { iocContainer } from '../ioc'
-import { BinanceListener } from './binance'
+// import { BinanceListener } from './binance'
 import { StellarListener } from './stellar'
 
 /**
@@ -11,8 +11,8 @@ export function startListeners(): void {
   iocContainer.bind<StellarListener>('StellarListener').to(StellarListener)
   iocContainer.get<StellarListener>('StellarListener').start()
 
-  iocContainer.bind<BinanceListener>('BinanceListener').to(BinanceListener)
-  iocContainer.get<BinanceListener>('BinanceListener').start()
+  // iocContainer.bind<BinanceListener>('BinanceListener').to(BinanceListener)
+  // iocContainer.get<BinanceListener>('BinanceListener').start()
 }
 
 if (require.main === module) {
