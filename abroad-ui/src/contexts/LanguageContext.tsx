b04de3e4af14f4f71383f1676ab026ad364efd16
Hollwann <hollwann@gmail.com>
2025-08-26 10:28:05 +0000

    Add LanguageDetector and LanguageStorage to tolgee initialization; update default language to 'en'

--- abroad-ui/src/contexts/LanguageContext.tsx
+++ abroad-ui/src/contexts/LanguageContext.tsx
@@ -1,13 +1,14 @@
-import { BackendFetch, DevTools, FormatSimple, Tolgee } from '@tolgee/react'
+import { BackendFetch, DevTools, FormatSimple, LanguageDetector, LanguageStorage, Tolgee } from '@tolgee/react'
 
 export const tolgee = Tolgee()
   .use(DevTools())
   .use(FormatSimple())
   .use(BackendFetch({ prefix: 'https://storage.googleapis.com/tolgee-cd-bucket/2e0e6ec0908462504864b33aed3a6846' }))
+  .use(LanguageDetector())
+  .use(LanguageStorage())
   .init({
     apiKey: import.meta.env.VITE_APP_TOLGEE_API_KEY,
     apiUrl: import.meta.env.VITE_APP_TOLGEE_API_URL,
     availableLanguages: ['pt', 'es', 'en', 'ru'],
-    defaultLanguage: 'pt',
-
+    defaultLanguage: 'en',
   })
