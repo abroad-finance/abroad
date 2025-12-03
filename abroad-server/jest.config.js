19a23eaa2ae48313281fd53a58da456619a6e386
Hollwann <hollwann@gmail.com>
2025-12-03 15:44:30 +0100

    feat: remove Guardline KYC service and related scripts, update test coverage requirement, and ignore routes from coverage.

--- abroad-server/jest.config.js
+++ abroad-server/jest.config.js
@@ -3,6 +3,9 @@ module.exports = {
   collectCoverageFrom: [
     'src/**/*.{js,jsx,ts,tsx}',
   ],
+  coveragePathIgnorePatterns: [
+    'src/routes.ts',
+  ],
   moduleFileExtensions: ['ts', 'js', 'json', 'node'],
   preset: 'ts-jest',
   testEnvironment: 'node',
