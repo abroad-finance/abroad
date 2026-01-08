2fce0127a455edee5964f4a2937951aea1b28267
Hollwann <hollwann@gmail.com>
2026-01-08 15:10:54 -0500

    feat: add deposit verifier registry

---
+++ abroad-server/src/modules/payments/application/DepositVerifierRegistry.ts
@@ -0,0 +1,27 @@
+import { BlockchainNetwork } from '@prisma/client'
+import { inject, injectable, multiInject, optional } from 'inversify'
+
+import { TYPES } from '../../../app/container/types'
+import { IDepositVerifier, IDepositVerifierRegistry } from './contracts/IDepositVerifier'
+
+@injectable()
+export class DepositVerifierRegistry implements IDepositVerifierRegistry {
+  private readonly verifiers: Map<BlockchainNetwork, IDepositVerifier>
+
+  constructor(
+    @multiInject(TYPES.IDepositVerifier) @optional() verifiers: IDepositVerifier[] = [],
+  ) {
+    this.verifiers = new Map()
+    for (const verifier of verifiers) {
+      this.verifiers.set(verifier.supportedNetwork, verifier)
+    }
+  }
+
+  public getVerifier(blockchain: BlockchainNetwork): IDepositVerifier {
+    const verifier = this.verifiers.get(blockchain)
+    if (!verifier) {
+      throw new Error(`No deposit verifier registered for ${blockchain}`)
+    }
+    return verifier
+  }
+}
